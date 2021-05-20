ef30ecfa9c368bda922bb5f463e75098ae090105
NarenCs-19 <narendirakumar2002@gmail.com>
2021-05-20 16:38:09 +0530

    updated the scrollable feature

--- JS/chat-box.js
+++ JS/chat-box.js
@@ -1,61 +1,81 @@
-
-//displaying the friend name if clicked
 $(document).ready(function(){
+
+    //displaying the friend name if clicked
     $(".friends-list").on("click",".frnd",function(){
         var user_id = $(this).children(".info").text();
-        const chat = $(".chat-box");
+        const chat_box = $(".chat-box");
         var init_info = $(".initial-info");
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "../backend/chat-box.php", true);
         xhr.onload = ()=>{
         if(xhr.readyState == 4 && xhr.status){
                 let data = xhr.response;
-                    chat.html(data);
-                    init_info.css("display","none");
+                chat_box.html(data);
+                init_info.css("display","none");
+                    
+                    const chat = $(".chat-box").find(".chats");
+                    var receiver_id = $(".chat-box").find(".user_id").text();
+                    //console.log(receiver_id);
+                    let Xhr = new XMLHttpRequest(); 
+                    Xhr.open("POST", "../backend/display-msg.php", true);
+                    Xhr.onload = ()=>{
+                        if(Xhr.readyState == 4 && xhr.status){
+                            let data = Xhr.response;
+                            chat.html(data);
+                            
+                            //console.log($(".chat-box .chats")[0].scrollHeight);
+                            //scrolling the chats to the bottom
+                            $(".chat-box").animate({scrollTop: $(".chat-box")[0].scrollHeight},1000);
+                        }
+                    }
+                    var dat = new FormData();
+                    dat.append('receiver_id',receiver_id);
+                    Xhr.send(dat);
+                    
             }
         }
         var dat = new FormData();
         dat.append('user_id',user_id);
         xhr.send(dat);
+
     });
 
-//sending the user typed message to php
-$(".chat-box").on('click',".text-bar .msg-send-icon",function(){
-    var msg = $(".text-bar textarea");
-    var receiver_id = $(".chat-box .sender-name .user_id").text();
-    console.log(receiver_id);
-    let xhr = new XMLHttpRequest(); 
-    xhr.open("POST", "../backend/insert-msg.php", true);
-    xhr.onload = ()=>{
-        if(xhr.readyState == 4 && xhr.status){
-            let data = xhr.response;
-            if(data === "success"){
-                msg.val("");
+    //sending the user typed message to php
+    $(".chat-box").on('click',".text-bar .msg-send-icon",function(){
+        var msg = $(".text-bar textarea");
+        var receiver_id = $(".chat-box .sender-name .user_id").text();
+        console.log(receiver_id);
+        if(msg.val().trim()!=""){
+            let xhr = new XMLHttpRequest(); 
+            xhr.open("POST", "../backend/insert-msg.php", true);
+            xhr.onload = ()=>{
+                if(xhr.readyState == 4 && xhr.status){
+                    let data = xhr.response;
+                    if(data === "success"){
+                        msg.val("");
+
+                        //getting the chats dynamically
+                        const chat = $(".chat-box").find(".chats");
+                        let xhr = new XMLHttpRequest(); 
+                        xhr.open("POST", "../backend/display-msg.php", true);
+                        xhr.onload = ()=>{
+                            if(xhr.readyState == 4 && xhr.status){
+                                let data = xhr.response;
+                                chat.html(data);
+                            }
+                        }
+                        var dat = new FormData();
+                        dat.append('receiver_id',receiver_id);
+                        xhr.send(dat);
+                    }
+                }
             }
+            var dat = new FormData();
+            dat.append('msg',msg.val());
+            dat.append('receiver_id',receiver_id);
+            xhr.send(dat);
         }
-    }
-    var dat = new FormData();
-    dat.append('msg',msg.val());
-    dat.append('receiver_id',receiver_id);
-    xhr.send(dat);
-});
-
-//getting the chats dynamically
-setInterval(()=>{
-    const chat = $(".chat-box").find(".chats");
-    var receiver_id = $(".chat-box").find(".user_id").text();
-    //console.log(receiver_id);
-    let xhr = new XMLHttpRequest(); 
-    xhr.open("POST", "../backend/display-msg.php", true);
-    xhr.onload = ()=>{
-        if(xhr.readyState == 4 && xhr.status){
-            let data = xhr.response;
-            chat.html(data);
-        }
-    }
-    var dat = new FormData();
-    dat.append('receiver_id',receiver_id);
-    xhr.send(dat);
-}, 200);
-
+        //scrolling the chats to the bottom
+        $(".chat-box").animate({scrollTop: $(".chats")[0].scrollHeight},1000);
+    });
 });
