db147806d40ae3630c91351879f8ed1cd8451b7e
Rajagnanaguru <rajagnanaguru@gmail.com>
2021-05-21 13:57:47 +0530

    Chat box conflict solving

--- JS/chat-box.js
+++ JS/chat-box.js
@@ -22,8 +22,7 @@ $(document).ready(function(){
                         if(Xhr.readyState == 4 && xhr.status){
                             let data = Xhr.response;
                             chat.html(data);
-                            
-                            //console.log($(".chat-box .chats")[0].scrollHeight);
+                            console.log($(".chat-box .chats")[0].scrollHeight);
                             //scrolling the chats to the bottom
                             $(".chat-box").animate({scrollTop: $(".chat-box")[0].scrollHeight},1000);
                         }
@@ -40,29 +39,20 @@ $(document).ready(function(){
 
     });
 
-//sending the user typed message to php
-$(".chat-box").on('click',".text-bar .msg-send-icon",function(){
-    var msg = $(".text-bar textarea");
-    var receiver_id = $(".chat-box .sender-name .user_id").text();
-    console.log(msg.val());
-    if (msg.val().trim()!='') {
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", "../backend/insert-msg.php", true);
-        xhr.onload = () => {
-            if (xhr.readyState == 4 && xhr.status) {
-                let data = xhr.response;
-                if (data === "success") {
-                    msg.val("");
-                }
-            }
-        }
-        var dat = new FormData();
-        dat.append('msg', msg.val());
-        dat.append('receiver_id', receiver_id);
-        xhr.send(dat);
-    }
-});
-    setInterval(()=>{
+    //sending the user typed message to php
+    $(".chat-box").on('click',".text-bar .msg-send-icon",function(){
+        var msg = $(".text-bar textarea");
+        var receiver_id = $(".chat-box .sender-name .user_id").text();
+        console.log(receiver_id);
+        if(msg.val().trim()!=""){
+            let xhr = new XMLHttpRequest(); 
+            xhr.open("POST", "../backend/insert-msg.php", true);
+            xhr.onload = ()=>{
+                if(xhr.readyState == 4 && xhr.status){
+                    let data = xhr.response;
+                    if(data === "success"){
+                        msg.val("");
+
                         //getting the chats dynamically
                         const chat = $(".chat-box").find(".chats");
                         let xhr = new XMLHttpRequest(); 
@@ -87,4 +77,41 @@ $(".chat-box").on('click',".text-bar .msg-send-icon",function(){
         //scrolling the chats to the bottom
         $(".chat-box").animate({scrollTop: $(".chats")[0].scrollHeight},1000);
     });
+
+    //dynamically getting the messages
+    setInterval(()=>{
+        var receiver_id = $(".chat-box .sender-name .user_id").text();
+        const chat = $(".chat-box").find(".chats");
+        let xhr = new XMLHttpRequest(); 
+        xhr.open("POST", "../backend/display-msg.php", true);
+        xhr.onload = ()=>{
+            if(xhr.readyState == 4 && xhr.status){
+                let data = xhr.response;
+                chat.html(data);
+            }
+        }
+        var dat = new FormData();
+        dat.append('receiver_id',receiver_id);
+        xhr.send(dat);
+    },200);
+
+    
+    //frequently updating the online status
+    setInterval(()=>{
+        var receiver_id = $(".chat-box .sender-name .user_id").text();
+        const status = $(".chat-box").find(".chat-box-header .online-status");
+        let xhr = new XMLHttpRequest(); 
+        xhr.open("POST", "../backend/online_status.php", true);
+        xhr.onload = ()=>{
+            if(xhr.readyState == 4 && xhr.status){
+                let data = xhr.response;
+                console.log(data);
+                status.text(data);
+            }
+        }
+        var dat = new FormData();
+        dat.append('receiver_id',receiver_id);
+        xhr.send(dat);
+    },500);
+
 });
